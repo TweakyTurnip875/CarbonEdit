86f5c9457ea730ccd5015ffa3a691296196b777f
404kevnotfound <44481502+TweakyTurnip875@users.noreply.github.com>
2024-04-09 09:58:05 -0600

    Created outline for the File menu item.

--- src/com/carbonedit/main/Main.java
+++ src/com/carbonedit/main/Main.java
@@ -8,10 +8,29 @@ import javax.swing.plaf.metal.*;
 import javax.swing.text.*;
 
 public class Main extends JFrame implements ActionListener {
+	
+	public Main() {
+		JFrame frame = new JFrame("CarbonEdit");
+		JTextArea textArea = new JTextArea();
+		JMenuBar menuBar = new JMenuBar();
+		
+		JMenu fileMenu = new JMenu("File");
+		
+		JMenuItem newFileMenuItem = new JMenuItem("New");
+		JMenuItem editFileMenuItem = new JMenuItem("Edit");
+		JMenuItem openFileMenuItem = new JMenuItem("Open");
+		JMenuItem saveFileMenuItem = new JMenuItem("Save");
+		JMenuItem saveAsFileMenuItem = new JMenuItem("Save As");
+		
+		fileMenu.add(newFileMenuItem);
+		fileMenu.add(editFileMenuItem);
+		fileMenu.add(openFileMenuItem);
+		fileMenu.add(saveFileMenuItem);
+		fileMenu.add(saveAsFileMenuItem);
+	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		
 	}
-	
 }
