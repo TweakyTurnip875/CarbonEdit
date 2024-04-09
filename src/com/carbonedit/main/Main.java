9ff680a51d4c808394ef7e5b65c63241c227548f
404kevnotfound <44481502+TweakyTurnip875@users.noreply.github.com>
2024-04-09 09:45:32 -0600

    Initial commit

---
+++ src/com/carbonedit/main/Main.java
@@ -0,0 +1,17 @@
+package com.carbonedit.main;
+
+import java.awt.*;
+import javax.swing.*;
+import java.io.*;
+import java.awt.event.*;
+import javax.swing.plaf.metal.*;
+import javax.swing.text.*;
+
+public class Main extends JFrame implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+	}
+	
+}
