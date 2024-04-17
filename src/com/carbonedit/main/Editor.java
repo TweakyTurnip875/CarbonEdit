b734f67865e7718b210d765100f73b6162471429
404kevnotfound <44481502+TweakyTurnip875@users.noreply.github.com>
2024-04-17 09:41:46 -0600

    Removed edit menu item in the file dropdown.
    There is already a edit menu dropdown, so it was redundant to have an edit button in the file menu.

--- src/com/carbonedit/main/Editor.java
+++ src/com/carbonedit/main/Editor.java
@@ -26,13 +26,11 @@ public class Editor extends JFrame implements ActionListener {
 		JMenu fileMenu = new JMenu("File");
 		
 		JMenuItem newFileMenuItem = new JMenuItem("New");
-		JMenuItem editFileMenuItem = new JMenuItem("Edit");
 		JMenuItem openFileMenuItem = new JMenuItem("Open");
 		JMenuItem saveFileMenuItem = new JMenuItem("Save");
 		JMenuItem saveAsFileMenuItem = new JMenuItem("Save As");
 		
 		fileMenu.add(newFileMenuItem);
-		fileMenu.add(editFileMenuItem);
 		fileMenu.add(openFileMenuItem);
 		fileMenu.add(saveFileMenuItem);
 		fileMenu.add(saveAsFileMenuItem);
